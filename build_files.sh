678ffc0e425edfa50d7934385247106c182fbb22
m.alramaly <mohamedalramally@gmail.com>
2026-09-11 05:35:28 +0300

    fix: use --break-system-packages for externally-managed pip

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 set -e 
-python3 -m pip install -r requirements.txt 
+python3 -m pip install --break-system-packages -r requirements.txt 
 python3 manage.py collectstatic --noinput --clear
